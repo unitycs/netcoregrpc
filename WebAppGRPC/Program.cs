7fbc735e95f8470868738bdb45d43782466b8f42
Edward <chenlulei@gmail.com>
2020-02-22 16:00:42 +0800

    support grpc

--- WebAppGRPC/Program.cs
+++ WebAppGRPC/Program.cs
@@ -1,15 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace WebAppGRPC
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -20,6 +22,16 @@ namespace WebAppGRPC
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    //An Optional to config grpc kestrel
+                    //webBuilder.ConfigureKestrel(options =>
+                    //{
+                    //    options.Listen(IPAddress.Any, 5001, listenOptions =>
+                    //    {
+                    //        listenOptions.Protocols = HttpProtocols.Http2;
+                    //        listenOptions.UseHttps("<path to .pfx file>",
+                    //            "<certificate password>");
+                    //    });
+                    //});
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- WebAppGRPC/Startup.cs
+++ WebAppGRPC/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebAppGRPC.GrpcServices;
 
 namespace WebAppGRPC
 {
@@ -19,6 +21,7 @@ namespace WebAppGRPC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddGrpc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +37,17 @@ namespace WebAppGRPC
             app.UseRouting();
 
             app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<SuckCatService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+
 
             app.UseEndpoints(endpoints =>
             {
